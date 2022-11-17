d24005183fb51338f81f4469ab55cc90b82df42d
Manu <admin@studiowan.fr>
2022-11-17 17:14:30 +0100

    migrate
    bin/rails db:migrate

---
+++ db/migrate/20221117155704_create_authors.rb
@@ -0,0 +1,11 @@
+class CreateAuthors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :authors do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.timestamps
+    end
+    add_index :authors, :email, unique: true
+  end
+end
