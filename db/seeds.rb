82c9b9fa1b7279b8e92bb00464b33e33db802151
Manu <admin@studiowan.fr>
2022-11-17 17:15:18 +0100

    Update seeds.rb
    bin/rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Inside of db/seeds.rb
+author1 = Author.create(first_name: "Tom", last_name: "Pratt", email: "tp@gmail.com")
+author2 = Author.create(first_name: "John", last_name: "Smith", email: "js@gmail.com")
+author3 = Author.create(first_name: "Jane", last_name: "Doe", email: "jd@gmail.com")
+book1 = Book.create(title: "The Great Gatsby", author_id: author1.id)
+book2 = Book.create(title: "The Catcher in the Rye", author_id: author1.id)
+book3 = Book.create(title: "The Grapes of Wrath", author_id: author2.id)
